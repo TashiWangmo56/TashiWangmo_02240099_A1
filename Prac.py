3deaace6f08be5809981a7641e46e462ea729b47
02240099 <02240099.cst@rub.edu.bt>
2025-03-04 00:06:16 +0600

    My Second commit

--- Prac.py
+++ Prac.py
@@ -1,4 +1,4 @@
 studentName = "Leki Wangmo"
 studentNumber = "02240093"
 
-print("My name is " + studentName + "and my student number is" + studentNumber)
+print("My name is " + studentName + " and my student number is " + studentNumber)
